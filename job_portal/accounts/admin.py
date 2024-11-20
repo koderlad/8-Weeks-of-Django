87ff9e3c744f3f39a7a6b8fe8267e3b7dafcdf7b
Kamlesh Shrestha <thekamlesh@Kamleshs-MacBook-Pro.local>
2024-11-20 21:29:21 +0545

    Created a model and migrated it to the database

--- job_portal/accounts/admin.py
+++ job_portal/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Candidate
 
-# Register your models here.
+# Register your models here. This allows to control the models via Django's built-in admin interface
+admin.site.register(Candidate)
--- job_portal/accounts/models.py
+++ job_portal/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#Creating Candidate Model
+class Candidate(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    resume = models.FileField(upload_to='resues/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # A String representation for the admin panel and debugging.
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
