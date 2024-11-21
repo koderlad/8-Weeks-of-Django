7925ed8e44c3753e5a6d3984e3a91934778893e9
Kamlesh Shrestha <thekamlesh@Kamleshs-MacBook-Pro.local>
2024-11-21 21:48:35 +0545

    Created a new Model: Employer and modified the previous model. Made few improvements in code, such as customizing Admin Labels for each model and registering the model for Admin Panel

--- job_portal/accounts/admin.py
+++ job_portal/accounts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Candidate
+from .models import Candidate, Employer
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email')
+
+class EmployerAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'contact_email', 'location')
+    search_fields = ('company_name', 'contact_email')
+
 
 # Register your models here. This allows to control the models via Django's built-in admin interface
-admin.site.register(Candidate)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Employer, EmployerAdmin)
--- job_portal/accounts/models.py
+++ job_portal/accounts/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 # Create your models here.
-#Creating Candidate Model
+
+#Employer models
+class Employer(models.Model):
+    company_name = models.CharField(max_length=200)
+    contact_email = models.EmailField(unique=True)
+    location = models.CharField(max_length=100)
+    website = models.URLField(max_length=200, blank=True, null=True)
+
+    def __self__(self):
+        return self.company_name
+
+#Candidate Model
 class Candidate(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    resume = models.FileField(upload_to='resues/', null=True, blank=True)
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+    employer = models.ForeignKey(Employer, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     # A String representation for the admin panel and debugging.
